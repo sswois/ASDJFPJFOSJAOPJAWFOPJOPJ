e38c7b116ada8dc95113bd36a0c9278e9f277457
sswois <113117797+sswois@users.noreply.github.com>
2025-06-27 17:10:36 -0800

    Update abc.lua

--- abc.lua
+++ abc.lua
@@ -2,7 +2,7 @@ local CoreGui = game:GetService("CoreGui")
 local UIS = game:GetService("UserInputService")
 
 local Jello = {}
-local tabCount = 0
+local TabCount = 0
 
 if CoreGui:FindFirstChild("Jello") then
 	return
@@ -23,7 +23,7 @@ ModalButton.Modal = false
 ModalButton.Parent = ScreenGui
 
 local BlurEffect = Instance.new("BlurEffect")
-BlurEffect.Size = 10
+BlurEffect.Size = 25
 BlurEffect.Enabled = false
 BlurEffect.Parent = game.Lighting
 
@@ -35,33 +35,33 @@ UIS.InputBegan:Connect(function(Input)
 	end
 end)
 
-function Jello:AddTab(tabName)
+function Jello:AddTab(TabName)
 	local TabFrame = Instance.new("Frame")
 	TabFrame.Parent = ScreenGui
 	TabFrame.BackgroundTransparency = 1
-	TabFrame.Position = UDim2.new(0, 15 + tabCount * 260, 0, 15)
+	TabFrame.Position = UDim2.new(0, 15 + TabCount * 260, 0, 15)
 	TabFrame.Size = UDim2.new(0, 250, 0, 0)
 	TabFrame.AutomaticSize = Enum.AutomaticSize.Y
 	TabFrame.BorderSizePixel = 0
 	TabFrame.BorderColor3 = Color3.new(0, 0, 0)
-	tabCount += 1
+	TabCount += 1
 
-	local TabHeader = Instance.new("TextButton")
-	TabHeader.Parent = TabFrame
-	TabHeader.BackgroundColor3 = Color3.new(0, 0, 0)
-	TabHeader.BackgroundTransparency = 0.25
-	TabHeader.BorderSizePixel = 0
-	TabHeader.BorderColor3 = Color3.new(0, 0, 0)
-	TabHeader.Size = UDim2.new(0, 250, 0, 50)
-	TabHeader.AutoButtonColor = false
-	TabHeader.Font = Enum.Font.Sarpanch
-	TabHeader.Text = tabName or "Tab"
-	TabHeader.TextColor3 = Color3.new(1, 1, 1)
-	TabHeader.TextSize = 25
-	TabHeader.TextXAlignment = Enum.TextXAlignment.Left
+	local Header = Instance.new("TextButton")
+	Header.Parent = TabFrame
+	Header.BackgroundColor3 = Color3.new(0, 0, 0)
+	Header.BackgroundTransparency = 0.25
+	Header.BorderSizePixel = 0
+	Header.BorderColor3 = Color3.new(0, 0, 0)
+	Header.Size = UDim2.new(0, 250, 0, 50)
+	Header.AutoButtonColor = false
+	Header.Font = Enum.Font.Sarpanch
+	Header.Text = TabName or "Tab"
+	Header.TextColor3 = Color3.new(1, 1, 1)
+	Header.TextSize = 25
+	Header.TextXAlignment = Enum.TextXAlignment.Left
 
-	local headerPadding = Instance.new("UIPadding", TabHeader)
-	headerPadding.PaddingLeft = UDim.new(0, 25)
+	local HeaderPadding = Instance.new("UIPadding", Header)
+	HeaderPadding.PaddingLeft = UDim.new(0, 25)
 
 	local Toggles = Instance.new("Frame")
 	Toggles.Parent = TabFrame
@@ -73,11 +73,11 @@ function Jello:AddTab(tabName)
 	Toggles.Visible = false
 	Toggles.AutomaticSize = Enum.AutomaticSize.Y
 
-	local layout = Instance.new("UIListLayout", Toggles)
-	layout.SortOrder = Enum.SortOrder.LayoutOrder
-	layout.Padding = UDim.new(0, 0)
+	local TogglesListLayout = Instance.new("UIListLayout", Toggles)
+	TogglesListLayout.SortOrder = Enum.SortOrder.LayoutOrder
+	TogglesListLayout.Padding = UDim.new(0, 0)
 
-	TabHeader.MouseButton2Click:Connect(function()
+	Header.MouseButton2Click:Connect(function()
 		Toggles.Visible = not Toggles.Visible
 	end)
 
@@ -92,9 +92,9 @@ function Jello:AddTab(tabName)
 		ToggleContainer.Size = UDim2.new(1, 0, 0, 0)
 		ToggleContainer.AutomaticSize = Enum.AutomaticSize.Y
 
-		local toggleLayout = Instance.new("UIListLayout", ToggleContainer)
-		toggleLayout.SortOrder = Enum.SortOrder.LayoutOrder
-		toggleLayout.Padding = UDim.new(0, 0)
+		local ToggleContainerListLayout = Instance.new("UIListLayout", ToggleContainer)
+		ToggleContainerListLayout.SortOrder = Enum.SortOrder.LayoutOrder
+		ToggleContainerListLayout.Padding = UDim.new(0, 0)
 
 		local Toggle = Instance.new("TextButton")
 		Toggle.Parent = ToggleContainer
@@ -111,8 +111,8 @@ function Jello:AddTab(tabName)
 		Toggle.TextTransparency = 0.5
 		Toggle.TextXAlignment = Enum.TextXAlignment.Left
 
-		local cheatPadding = Instance.new("UIPadding", Toggle)
-		cheatPadding.PaddingLeft = UDim.new(0, 25)
+		local TogglePadding = Instance.new("UIPadding", Toggle)
+		TogglePadding.PaddingLeft = UDim.new(0, 25)
 
 		local ToggleFeatures = Instance.new("Frame")
 		ToggleFeatures.Parent = ToggleContainer
@@ -125,9 +125,9 @@ function Jello:AddTab(tabName)
 		ToggleFeatures.AutomaticSize = Enum.AutomaticSize.Y
 		ToggleFeatures.ClipsDescendants = true
 
-		local featuresLayout = Instance.new("UIListLayout", ToggleFeatures)
-		featuresLayout.SortOrder = Enum.SortOrder.LayoutOrder
-		featuresLayout.Padding = UDim.new(0, 5)
+		local ToggleFeaturesListLayout = Instance.new("UIListLayout", ToggleFeatures)
+		ToggleFeaturesListLayout.SortOrder = Enum.SortOrder.LayoutOrder
+		ToggleFeaturesListLayout.Padding = UDim.new(0, 5)
 
 		local Bind = Instance.new("TextButton")
 		Bind.Parent = ToggleFeatures
@@ -141,18 +141,24 @@ function Jello:AddTab(tabName)
 		Bind.TextSize = 15
 		Bind.TextXAlignment = Enum.TextXAlignment.Left
 
-		local bindPadding = Instance.new("UIPadding", Bind)
-		bindPadding.PaddingLeft = UDim.new(0, 25)
+		local BindPadding = Instance.new("UIPadding", Bind)
+		BindPadding.PaddingLeft = UDim.new(0, 25)
 
-		local enabled = false
-		local currentBind = nil
-		local binding = false
-		local skipNext = false
+		local Enabled = false
+		local CurrentBind = nil
+		local Binding = false
+		local SkipNext = false
+
+		function Toggle()
+			Enabled = not Enabled
+			Toggle.TextTransparency = Enabled and 0 or 0.5
+			if callback then
+				callback(Enabled)
+			end
+		end
 
 		Toggle.MouseButton1Click:Connect(function()
-			enabled = not enabled
-			Toggle.TextTransparency = enabled and 0 or 0.5
-			if callback then callback(enabled) end
+			Toggle()
 		end)
 
 		Toggle.MouseButton2Click:Connect(function()
@@ -160,40 +166,38 @@ function Jello:AddTab(tabName)
 		end)
 
 		Bind.MouseButton1Click:Connect(function()
-			if binding then return end
-			binding = true
+			if Binding then
+				return
+			end
+			Binding = true
 			Bind.Text = "Press Key"
-
-			local conn
-			conn = UIS.InputBegan:Connect(function(Input)
+			BindConnection = UIS.InputBegan:Connect(function(Input)
 				if Input.UserInputType == Enum.UserInputType.Keyboard then
-					if currentBind == Input.KeyCode then
-						currentBind = nil
+					if CurrentBind == Input.KeyCode then
+						CurrentBind = nil
 						Bind.Text = "Bind Removed"
 						task.delay(1, function()
 							Bind.Text = "Bind: None"
 						end)
-						skipNext = true
+						SkipNext = true
 					else
-						currentBind = Input.KeyCode
+						CurrentBind = Input.KeyCode
 						Bind.Text = "Bind: " .. Input.KeyCode.Name
-						skipNext = true
+						SkipNext = true
 					end
-					conn:Disconnect()
-					binding = false
+					BindConnection:Disconnect()
+					Binding = false
 				end
 			end)
 		end)
 
 		UIS.InputBegan:Connect(function(Input)
-			if currentBind == Input.KeyCode then
-				if skipNext then
-					skipNext = false
+			if CurrentBind == Input.KeyCode then
+				if SkipNext then
+					SkipNext = false
 					return
 				end
-				enabled = not enabled
-				Toggle.TextTransparency = enabled and 0 or 0.5
-				if callback then callback(enabled) end
+				Toggle()
 			end
 		end)
 	end
