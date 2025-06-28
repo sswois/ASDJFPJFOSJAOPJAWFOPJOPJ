823258cf12616b82e2b320aba050e62313b73ebb
sswois <113117797+sswois@users.noreply.github.com>
2025-06-28 14:06:59 -0800

    Update abc.lua

--- abc.lua
+++ abc.lua
@@ -61,7 +61,7 @@ local function RefreshActiveModules()
 	for _, ModuleName in ipairs(ActiveModules) do
 		local Label = Instance.new("TextLabel")
 		Label.Parent = ActiveModulesDisplay
-		Label.BackgroundTransparency = 0.75
+		Label.BackgroundTransparency = 0.5
 		Label.BackgroundColor3 = Color3.new(0, 0, 0)
 		Label.BorderColor3 = Color3.new(0, 0, 0)
 		Label.BorderSizePixel = 0
